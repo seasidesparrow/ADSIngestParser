13947ab64f999107c0f4090e6ddf9b2e5477f9f4
Kelly Lockhart <2926089+kelockhart@users.noreply.github.com>
2022-02-25 15:39:35 -0500

    Initial move to cookiecutter/acutter template

---
+++ adsingestp/cli.py
@@ -0,0 +1,27 @@
+import click
+
+try:
+    import rutils
+
+    config = rutils.load_config()
+    logger = rutils.setup_logging("adsingestp.cli")
+except ImportError:
+    import logging
+
+    config = {}
+    logger = logging.getLogger("adsingestp.cli")
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def hello():
+    """Will greet"""
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    cli()
