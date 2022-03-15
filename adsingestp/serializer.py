8f0a3767e8e4b9035855fb1ccb821ff64321be0b
Kelly Lockhart <2926089+kelockhart@users.noreply.github.com>
2022-03-15 15:51:57 -0400

    Edits to get pre-commit tests to pass (pep8, etc.)

--- adsingestp/serializer.py
+++ adsingestp/serializer.py
@@ -3,12 +3,15 @@ from datetime import datetime
 TIMESTAMP_FMT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 # TODO will need to add boolean keys here if we want to keep falsy values - check these
-required_keys = ['createdTime',
-                 'parsedTime',
-                 'loadType',
-                 'loadFormat',
-                 'loadLocation',
-                 'recordOrigin']
+required_keys = [
+    "createdTime",
+    "parsedTime",
+    "loadType",
+    "loadFormat",
+    "loadLocation",
+    "recordOrigin",
+]
+
 
 def clean_empty(input_to_clean, keys_to_keep=required_keys):
     """
@@ -21,7 +24,7 @@ def clean_empty(input_to_clean, keys_to_keep=required_keys):
     if isinstance(input_to_clean, dict):
         return {
             k: v
-            for k, v in ((k, clean_empty(v)) for k,v in input_to_clean.items())
+            for k, v in ((k, clean_empty(v)) for k, v in input_to_clean.items())
             if v or (k in keys_to_keep)
         }
 
@@ -29,6 +32,7 @@ def clean_empty(input_to_clean, keys_to_keep=required_keys):
         return [v for v in map(clean_empty, input_to_clean) if v]
 
     return input_to_clean
+
 
 def serialize(input_dict, format):
     """
@@ -38,120 +42,131 @@ def serialize(input_dict, format):
     :return: serialized JSON that follows our internal data model
     """
 
-    if format not in ['JATS', 'OtherXML', 'HTML', 'Text']:
+    if format not in ["JATS", "OtherXML", "HTML", "Text"]:
         # TODO give this a real exception
         raise Exception
 
     output = {}
 
-    output['recordData'] = {
-        'createdTime': '',
-        'parsedTime': datetime.utcnow().strftime(TIMESTAMP_FMT),
-        'loadType': 'fromURL' if format=='HTML' else 'fromFile',
-        'loadFormat': format,
-        'loadLocation': '',
-        'recordOrigin': ''
+    output["recordData"] = {
+        "createdTime": "",
+        "parsedTime": datetime.utcnow().strftime(TIMESTAMP_FMT),
+        "loadType": "fromURL" if format == "HTML" else "fromFile",
+        "loadFormat": format,
+        "loadLocation": "",
+        "recordOrigin": "",
     }
     # TODO this should be an array of objects - talk to MT
     # TODO once we can parse more than just errata, need to expand this
-    if 'erratum' in input_dict:
-        output['relatedTo'] = {
-            'relationship': 'errata',
-            'relatedDocID': input_dict['erratum']
-        }
+    if "erratum" in input_dict:
+        output["relatedTo"] = {"relationship": "errata", "relatedDocID": input_dict["erratum"]}
 
-    output['editorialHistory'] = {
-        'receivedDates': input_dict.get('edhist_rec', ''),
-        'revisedDates': input_dict.get('edhist_rev', ''),
-        'acceptedDate': input_dict.get('edhist_acc', '')
+    output["editorialHistory"] = {
+        "receivedDates": input_dict.get("edhist_rec", ""),
+        "revisedDates": input_dict.get("edhist_rev", ""),
+        "acceptedDate": input_dict.get("edhist_acc", ""),
     }
 
-    output['pubDate'] = {
-        'electrDate': input_dict.get('pubdate_electronic', ''),
-        'printDate': input_dict.get('pubdate_print', ''),
+    output["pubDate"] = {
+        "electrDate": input_dict.get("pubdate_electronic", ""),
+        "printDate": input_dict.get("pubdate_print", ""),
         # 'otherPubDate': {
         #     'otherDateType': 'XXX', # TODO
         #     'otherDate': 'XXX' # TODO
         # }
     }
 
-    output['publication'] = {
+    output["publication"] = {
         #'docType': 'XXX',
-        'pubName': input_dict.get('publication', ''),
+        "pubName": input_dict.get("publication", ""),
         #'confName': 'XXX',
         #'confLocation': 'XXX',
         #'confDates': 'XXX',
         #'confEditors': ['XXX'],
         #'confPID': 'XXX',
-        'publisher': input_dict.get('publisher', ''),
-        'issueNum': input_dict.get('issue', ''),
-        'volumeNum': input_dict.get('volume', ''),
-        'pubYear': input_dict['pubdate_print'][0:4] if 'pubdate_print' in input_dict
-        else (input_dict['pubdate_electronic'][0:4] if 'pubdate_electronic' in input_dict else ''),
-        'ISSN': [{'pubtype': pubtype, 'issnString': issn} for (pubtype, issn) in input_dict['issn']] if 'issn' in input_dict else '',
+        "publisher": input_dict.get("publisher", ""),
+        "issueNum": input_dict.get("issue", ""),
+        "volumeNum": input_dict.get("volume", ""),
+        "pubYear": input_dict["pubdate_print"][0:4]
+        if "pubdate_print" in input_dict
+        else (input_dict["pubdate_electronic"][0:4] if "pubdate_electronic" in input_dict else ""),
+        "ISSN": [
+            {"pubtype": pubtype, "issnString": issn} for (pubtype, issn) in input_dict["issn"]
+        ]
+        if "issn" in input_dict
+        else "",
         #'isRefereed': True or False
     }
 
-    output['persistentIDs'] = [
+    output["persistentIDs"] = [
         {
             #'Crossref': 'XXX',
             #'ISBN': 'XXX',
-            'DOI': input_dict['ids']['doi'] if 'ids' in input_dict and 'doi' in input_dict['ids'] else '',
-            'preprint': {
-                'source': input_dict['ids']['preprint']['source'] if 'ids' in input_dict
-                                                                     and 'preprint' in input_dict['ids']
-                                                                     and 'source' in input_dict['ids']['preprint'] else '',
-                'identifier': input_dict['ids']['preprint']['id'] if 'ids' in input_dict
-                                                                     and 'preprint' in input_dict['ids']
-                                                                     and 'id' in input_dict['ids']['preprint'] else ''
-            }
+            "DOI": input_dict["ids"]["doi"]
+            if "ids" in input_dict and "doi" in input_dict["ids"]
+            else "",
+            "preprint": {
+                "source": input_dict["ids"]["preprint"]["source"]
+                if "ids" in input_dict
+                and "preprint" in input_dict["ids"]
+                and "source" in input_dict["ids"]["preprint"]
+                else "",
+                "identifier": input_dict["ids"]["preprint"]["id"]
+                if "ids" in input_dict
+                and "preprint" in input_dict["ids"]
+                and "id" in input_dict["ids"]["preprint"]
+                else "",
+            },
         }
     ]
 
-    output['publisherIDs'] = [
-        {
-            'attribute': i.get('attribute', ''),
-            'Identifier': i.get('Identifier', '')
-        }
-    for i in input_dict['ids']['pub-id'] if 'ids' in input_dict and 'pub-id' in input_dict['ids']]
+    output["publisherIDs"] = [
+        {"attribute": i.get("attribute", ""), "Identifier": i.get("Identifier", "")}
+        for i in input_dict["ids"]["pub-id"]
+        if "ids" in input_dict and "pub-id" in input_dict["ids"]
+    ]
 
-    output['pagination'] = {
-        'firstPage': input_dict.get('page_first', ''),
-        'lastPage': input_dict.get('page_last', ''),
-        'pageCount': input_dict.get('numpages', ''),
-        'pageRange': input_dict.get('page_range', ''),
-        'electronicID': input_dict.get('electronic_id', '')
+    output["pagination"] = {
+        "firstPage": input_dict.get("page_first", ""),
+        "lastPage": input_dict.get("page_last", ""),
+        "pageCount": input_dict.get("numpages", ""),
+        "pageRange": input_dict.get("page_range", ""),
+        "electronicID": input_dict.get("electronic_id", ""),
     }
 
-    output['authors'] = [
+    output["authors"] = [
         {
-            'name': {
-                'surname': i.get('surname', ''),
-                'given-name': i.get('given', ''),
-                'middle-name': i.get('middle', ''),
-                'prefix': i.get('prefix', ''),
-                'suffix': i.get('suffix', ''),
-                'pubraw': i.get('nameraw', ''),
+            "name": {
+                "surname": i.get("surname", ""),
+                "given-name": i.get("given", ""),
+                "middle-name": i.get("middle", ""),
+                "prefix": i.get("prefix", ""),
+                "suffix": i.get("suffix", ""),
+                "pubraw": i.get("nameraw", ""),
                 #'native-lang': 'XXX',
                 #'collab': 'XXX' # TODO need a collab example
             },
-            'affiliation': [
+            "affiliation": [
                 {
-                    'affPubRaw': j,
-                    'affPubID': i['xaff'][idx],
+                    "affPubRaw": j,
+                    "affPubID": i["xaff"][idx],
                     #'affPubIDType': 'XXX' # TODO ask MT
                 }
-            for idx, j in enumerate(i['aff']) if 'aff' in i],
-            'attrib': {
+                for idx, j in enumerate(i["aff"])
+                if "aff" in i
+            ],
+            "attrib": {
                 #'collab': True or False, # TODO need a collab example
                 #'deceased': True or False, # TODO need an example
                 #'coauthor': True or False, # TODO need an example
-                'email': i.get('email', ''),
+                "email": i.get("email", ""),
                 #'funding': 'XXX', # TODO need an example
-                'orcid': i.get('orcid', ''),
-            }
+                "orcid": i.get("orcid", ""),
+            },
         }
-    for i in input_dict['authors'] if 'authors' in input_dict]
+        for i in input_dict["authors"]
+        if "authors" in input_dict
+    ]
 
     # output['otherContributor'] = [
     #     {
@@ -186,16 +201,20 @@ def serialize(input_dict, format):
     #     }
     # ] # TODO need an example
 
-    output['title'] = {
-        'textEnglish': input_dict.get('title', ''), # TODO need to tweak for case of foreign language title
+    output["title"] = {
+        "textEnglish": input_dict.get(
+            "title", ""
+        ),  # TODO need to tweak for case of foreign language title
         # 'textNative': 'XXX', # TODO
         # 'langNative': 'XXX' # TODO # TODO need an example
     }
 
-    #output['subtitle'] = 'XXX' # TODO need an example
+    # output['subtitle'] = 'XXX' # TODO need an example
 
-    output['abstract'] = {
-        'textEnglish': input_dict.get('abstract', ''), # TODO need to tweak for case of foreign language abstract
+    output["abstract"] = {
+        "textEnglish": input_dict.get(
+            "abstract", ""
+        ),  # TODO need to tweak for case of foreign language abstract
         #'textNative': 'XXX', # TODO
         #'langNative': 'XXX' # TODO
     }
@@ -212,9 +231,13 @@ def serialize(input_dict, format):
     #     'body': 'XXX'
     # } # TODO this is from fulltext
 
-    #output['acknowledgements'] = 'XXX' # TODO this is from fulltext
+    # output['acknowledgements'] = 'XXX' # TODO this is from fulltext
 
-    output['references'] = (input_dict['references'] if type(input_dict['references']) == list else [input_dict['references']]) or ''
+    output["references"] = (
+        input_dict["references"]
+        if type(input_dict["references"]) == list
+        else [input_dict["references"]]
+    ) or ""
 
     # output['backmatter'] = [
     #     {
@@ -245,34 +268,36 @@ def serialize(input_dict, format):
     #     }
     # ] # TODO need an example
 
-    #output['doctype'] = 'XXX' # TODO ask MT about this
+    # output['doctype'] = 'XXX' # TODO ask MT about this
 
-    output['keywords'] = [
+    output["keywords"] = [
         {
-            'keyString': i.get('string', ''),
-            'keySystem': i.get('system', ''),
+            "keyString": i.get("string", ""),
+            "keySystem": i.get("system", ""),
             # 'keyIdent': {
             #     'system': 'XXX',
             #     'keyID': 'XXX'
             # }
         }
-    for i in input_dict.get('keywords', []) ]
+        for i in input_dict.get("keywords", [])
+    ]
 
-    output['copyright'] = {
-        'status': True if 'copyright' in input_dict else False, # TODO ask MT about this
-        'statement': input_dict.get('copyright','')
+    output["copyright"] = {
+        "status": True if "copyright" in input_dict else False,  # TODO ask MT about this
+        "statement": input_dict.get("copyright", ""),
     }
 
-    output['openAccess'] = {
-        'open': input_dict['openAccess']['open'] if 'openAccess' in input_dict
-                                                    and 'open' in input_dict['openAccess'] else False,
+    output["openAccess"] = {
+        "open": input_dict["openAccess"]["open"]
+        if "openAccess" in input_dict and "open" in input_dict["openAccess"]
+        else False,
         # 'license': 'XXX',
         # 'licenseURL': 'XXX',
         # 'preprint': 'XXX',
         # 'startDate': 'XXX',
         # 'endDate': 'XXX',
         # 'embargoLength': 'XXX'
-    } # TODO need an example
+    }  # TODO need an example
 
     # output['pubnote'] = 'XXX' # TODO need an example
     #
--- adsingestp/utils.py
+++ adsingestp/utils.py
@@ -1,20 +1,32 @@
-import re
 import os
+import re
+
 from namedentities import named_entities
 
-re_ents = re.compile(r'&[a-z0-9]+;|&#[0-9]{1,6};|&#x[0-9a-fA-F]{1,6};')
+re_ents = re.compile(r"&[a-z0-9]+;|&#[0-9]{1,6};|&#x[0-9a-fA-F]{1,6};")
 
-MONTH_TO_NUMBER = {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'jun': 6,
-                   'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10, 'nov': 11,
-                   'dec': 12}
+MONTH_TO_NUMBER = {
+    "jan": 1,
+    "feb": 2,
+    "mar": 3,
+    "apr": 4,
+    "may": 5,
+    "jun": 6,
+    "jul": 7,
+    "aug": 8,
+    "sep": 9,
+    "oct": 10,
+    "nov": 11,
+    "dec": 12,
+}
 
 # HTML_ENTITY_TABLE
-HTML_ENTITY_TABLE = os.path.dirname(os.path.abspath(__file__)) + '/html5.dat'
+HTML_ENTITY_TABLE = os.path.dirname(os.path.abspath(__file__)) + "/html5.dat"
 ENTITY_DICTIONARY = dict()
 try:
-    with open(HTML_ENTITY_TABLE, 'r') as fent:
-        for l in fent.readlines():
-            carr = l.rstrip().split('\t')
+    with open(HTML_ENTITY_TABLE, "r") as fent:
+        for line in fent.readlines():
+            carr = line.rstrip().split("\t")
 
             UNI_ENTITY = None
             NAME_ENTITY = None
@@ -27,7 +39,7 @@ try:
                 DEC_ENTITY = carr[3].lower()
                 for c in NAME_ENTITY.strip().split():
                     # preserve greek letters, convert all other high-bit chars
-                    eValue = int(DEC_ENTITY.lstrip('&#').rstrip(';'))
+                    eValue = int(DEC_ENTITY.lstrip("&#").rstrip(";"))
                     if (eValue >= 913 and eValue <= 969) or (eValue >= 192 and eValue <= 382):
                         ENTITY_DICTIONARY[UNI_ENTITY.strip()] = c.strip()
                         ENTITY_DICTIONARY[HEX_ENTITY.strip()] = c.strip()
@@ -37,7 +49,7 @@ try:
                         ENTITY_DICTIONARY[HEX_ENTITY.strip()] = DEC_ENTITY.strip()
                         ENTITY_DICTIONARY[c.strip()] = DEC_ENTITY.strip()
             else:
-                print("broken HTML entity:", l.rstrip())
+                print("broken HTML entity:", line.rstrip())
                 NAME_ENTITY = "xxxxx"
 
 except Exception as e:
@@ -45,40 +57,40 @@ except Exception as e:
 
 # ADS-specific translations
 # have been added to html5.txt
-ENTITY_DICTIONARY['&sim;'] = "~"
-ENTITY_DICTIONARY['&#8764;'] = "~"
-ENTITY_DICTIONARY['&Tilde;'] = "~"
-ENTITY_DICTIONARY['&rsquo;'] = "'"
-ENTITY_DICTIONARY['&#8217;'] = "'"
-ENTITY_DICTIONARY['&lsquo;'] = "'"
-ENTITY_DICTIONARY['&#8216;'] = "'"
-ENTITY_DICTIONARY['&nbsp;'] = " "
-ENTITY_DICTIONARY['&mdash;'] = "-"
-ENTITY_DICTIONARY['&#8212;'] = "-"
-ENTITY_DICTIONARY['&ndash;'] = "-"
-ENTITY_DICTIONARY['&#8211;'] = "-"
-ENTITY_DICTIONARY['&rdquo;'] = '"'
-ENTITY_DICTIONARY['&#8221;'] = '"'
-ENTITY_DICTIONARY['&ldquo;'] = '"'
-ENTITY_DICTIONARY['&#8220;'] = '"'
-ENTITY_DICTIONARY['&minus;'] = "-"
-ENTITY_DICTIONARY['&#8722;'] = "-"
-ENTITY_DICTIONARY['&plus;'] = "+"
-ENTITY_DICTIONARY['&#43;'] = "+"
-ENTITY_DICTIONARY['&thinsp;'] = " "
-ENTITY_DICTIONARY['&#8201;'] = " "
-ENTITY_DICTIONARY['&hairsp;'] = " "
-ENTITY_DICTIONARY['&#8202;'] = " "
-ENTITY_DICTIONARY['&ensp;'] = " "
-ENTITY_DICTIONARY['&#8194;'] = " "
-ENTITY_DICTIONARY['&emsp;'] = " "
-ENTITY_DICTIONARY['&#8195;'] = " "
+ENTITY_DICTIONARY["&sim;"] = "~"
+ENTITY_DICTIONARY["&#8764;"] = "~"
+ENTITY_DICTIONARY["&Tilde;"] = "~"
+ENTITY_DICTIONARY["&rsquo;"] = "'"
+ENTITY_DICTIONARY["&#8217;"] = "'"
+ENTITY_DICTIONARY["&lsquo;"] = "'"
+ENTITY_DICTIONARY["&#8216;"] = "'"
+ENTITY_DICTIONARY["&nbsp;"] = " "
+ENTITY_DICTIONARY["&mdash;"] = "-"
+ENTITY_DICTIONARY["&#8212;"] = "-"
+ENTITY_DICTIONARY["&ndash;"] = "-"
+ENTITY_DICTIONARY["&#8211;"] = "-"
+ENTITY_DICTIONARY["&rdquo;"] = '"'
+ENTITY_DICTIONARY["&#8221;"] = '"'
+ENTITY_DICTIONARY["&ldquo;"] = '"'
+ENTITY_DICTIONARY["&#8220;"] = '"'
+ENTITY_DICTIONARY["&minus;"] = "-"
+ENTITY_DICTIONARY["&#8722;"] = "-"
+ENTITY_DICTIONARY["&plus;"] = "+"
+ENTITY_DICTIONARY["&#43;"] = "+"
+ENTITY_DICTIONARY["&thinsp;"] = " "
+ENTITY_DICTIONARY["&#8201;"] = " "
+ENTITY_DICTIONARY["&hairsp;"] = " "
+ENTITY_DICTIONARY["&#8202;"] = " "
+ENTITY_DICTIONARY["&ensp;"] = " "
+ENTITY_DICTIONARY["&#8194;"] = " "
+ENTITY_DICTIONARY["&emsp;"] = " "
+ENTITY_DICTIONARY["&#8195;"] = " "
+
 
 class EntityConverter(object):
-
     def __init__(self):
-        self.input_text = ''
-        self.output_text = ''
+        self.input_text = ""
+        self.output_text = ""
         self.ent_dict = ENTITY_DICTIONARY
 
     def convert(self):
@@ -88,7 +100,7 @@ class EntityConverter(object):
         for e in oents:
             try:
                 enew = self.ent_dict[e]
-            except:
+            except Exception:
                 pass
             else:
                 o = re.sub(e, enew, o)
