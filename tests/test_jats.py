caa03c6d1332f7e594b5a90a1fc1373c63d2ce45
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2023-08-09 13:07:08 -0400

    fix: added test case for jats record with subtitle  	new file:   tests/stubdata/input/jats_a+a_subtitle.xml  	new file:   tests/stubdata/output/jats_a+a_subtitle.json  	modified:   tests/test_jats.py

--- tests/test_jats.py
+++ tests/test_jats.py
@@ -41,6 +41,7 @@ class TestJATS(unittest.TestCase):
             "jats_iop_preprint_in_record",
             "jats_iop_apj_923_1_47",
             "jats_a+a_multiparagraph_abstract",
+            "jats_a+a_subtitle",
         ]
         for f in filenames:
             test_infile = os.path.join(self.inputdir, f + ".xml")
