0938a1ce5bd01f0b4ebefe67e2d69e75b5f1d32d
mugdhapolimera <35502000+mugdhapolimera@users.noreply.github.com>
2024-01-05 16:11:51 -0500

    Dublincore parser to replace arxiv parser (#84)
    * Creating generic dublincore parser
    
    * Replaced arxiv parser with dubcore parser
    
    * removed identifier from header and added publisher field
    
    ---------
    
    Co-authored-by: Mugdha Polimera <mugdhapolimera@saos-MacBook-Pro.local>
    Co-authored-by: Mugdha Polimera <mugdhapolimera@saos-mbp.lan>

--- adsingestp/parsers/dubcore.py
+++ adsingestp/parsers/dubcore.py
@@ -13,18 +13,18 @@ from adsingestp.parsers.base import BaseBeautifulSoupParser, IngestBase
 logger = logging.getLogger(__name__)
 
 
-class MultiArxivParser(IngestBase):
+class MultiDublinCoreParser(IngestBase):
     start_re = r"<record(?!-)[^>]*>"
     end_re = r"</record(?!-)[^>]*>"
 
     def parse(self, text, header=False):
         """
-        Separate multi-record arXiv XML document into individual XML documents
+        Separate multi-record DublinCore XML document into individual XML documents
 
         :param text: string, input XML text from a multi-record XML document
         :param header: boolean (default: False), set to True to preserve overall
             document header/footer for each separate record's document
-        :return: list, each item is the XML of a separate arXiv document
+        :return: list, each item is the XML of a separate DublinCore document
         """
         output_chunks = []
         for chunk in self.get_chunks(text, self.start_re, self.end_re, head_foot=header):
@@ -33,15 +33,14 @@ class MultiArxivParser(IngestBase):
         return output_chunks
 
 
-class ArxivParser(BaseBeautifulSoupParser):
-    # Dublin Core parser for arXiv
+class DublinCoreParser(BaseBeautifulSoupParser):
+    # Generic Dublin Core parser
 
     DUBCORE_SCHEMA = ["http://www.openarchives.org/OAI/2.0/oai_dc/"]
 
     author_collaborations_params = {
         "keywords": ["group", "team", "collaboration"],
         "remove_the": False,
-        "fix_arXiv_mixed_collaboration_string": True,
     }
 
     def __init__(self):
@@ -50,24 +49,14 @@ class ArxivParser(BaseBeautifulSoupParser):
         self.input_metadata = None
 
     def _parse_ids(self):
-        if self.input_header.find("identifier"):
-            ids = self.input_header.find("identifier").get_text()
-            id_array = ids.split(":")
-            arxiv_id = id_array[-1]
+        self.base_metadata["ids"] = {}
+        self.base_metadata["ids"]["pub-id"] = []
 
-            # TODO what should the key on this actually be?
-            self.base_metadata["publication"] = "eprint arXiv:" + arxiv_id
-
-            self.base_metadata["ids"] = {"preprint": {}}
-
-            self.base_metadata["ids"]["preprint"]["source"] = "arXiv"
-            self.base_metadata["ids"]["preprint"]["id"] = arxiv_id
-
-        dc_ids = self.input_metadata.find_all("dc:identifier")
-        for d in dc_ids:
-            d_text = d.get_text()
-            if "doi:" in d_text:
-                self.base_metadata["ids"]["doi"] = d_text.replace("doi:", "")
+        if self.input_metadata.find("dc:identifier"):
+            for dc_id in self.input_metadata.find_all("dc:identifier"):
+                self.base_metadata["ids"]["pub-id"].append(
+                    {"attribute": "publisher-id", "Identifier": dc_id.get_text()}
+                )
 
     def _parse_title(self):
         title_array = self.input_metadata.find_all("dc:title")
@@ -104,16 +93,21 @@ class ArxivParser(BaseBeautifulSoupParser):
                 "dc:date"
             ).get_text()
 
+    def _parse_publisher(self):
+        if self.input_metadata.find("dc:publisher"):
+            self.base_metadata["publisher"] = self.input_metadata.find("dc:publisher").get_text()
+
     def _parse_abstract(self):
         desc_array = self.input_metadata.find_all("dc:description")
-        # for arXiv.org, only 'dc:description'[0] is the abstract, the rest are comments
+        # in general, only 'dc:description'[0] is the abstract, the rest are comments
         if desc_array:
             self.base_metadata["abstract"] = self._clean_output(desc_array.pop(0).get_text())
 
         if desc_array:
             comments_out = []
             for d in desc_array:
-                comments_out.append({"origin": "arxiv", "text": self._clean_output(d.get_text())})
+                # TODO: FIX
+                comments_out.append({"text": self._clean_output(d.get_text())})
 
             self.base_metadata["comments"] = comments_out
 
@@ -123,12 +117,12 @@ class ArxivParser(BaseBeautifulSoupParser):
         if keywords_array:
             keywords_out = []
             for k in keywords_array:
-                keywords_out.append({"system": "arxiv", "string": k.get_text()})
+                keywords_out.append({"string": k.get_text()})
             self.base_metadata["keywords"] = keywords_out
 
     def parse(self, text):
         """
-        Parse arXiv XML into standard JSON format
+        Parse DublinCore XML into standard JSON format
         :param text: string, contents of XML file
         :return: parsed file contents in JSON format
         """
@@ -154,6 +148,7 @@ class ArxivParser(BaseBeautifulSoupParser):
         self._parse_pubdate()
         self._parse_abstract()
         self._parse_keywords()
+        self._parse_publisher()
 
         self.base_metadata = self._entity_convert(self.base_metadata)
 
--- tests/test_dublincore.py
+++ tests/test_dublincore.py
@@ -5,19 +5,20 @@ import unittest
 
 from adsingestschema import ads_schema_validator
 
-from adsingestp.parsers import arxiv
+from adsingestp.parsers import dubcore
 
 TIMESTAMP_FMT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 
-class TestArxiv(unittest.TestCase):
+class TestDublinCore(unittest.TestCase):
     def setUp(self):
         stubdata_dir = os.path.join(os.path.dirname(__file__), "stubdata/")
         self.inputdir = os.path.join(stubdata_dir, "input")
         self.outputdir = os.path.join(stubdata_dir, "output")
 
-    def test_arxiv(self):
+    def test_dubcore(self):
         filenames = [
+            "dubcore_pos_ecrs_002",
             "arxiv_1711_05739",
             "arxiv_0901_2443",
             "arxiv_1711_04702",
@@ -26,7 +27,7 @@ class TestArxiv(unittest.TestCase):
         for f in filenames:
             test_infile = os.path.join(self.inputdir, f + ".xml")
             test_outfile = os.path.join(self.outputdir, f + ".json")
-            parser = arxiv.ArxivParser()
+            parser = dubcore.DublinCoreParser()
 
             with open(test_infile, "rb") as fp:
                 input_data = fp.read()
@@ -55,18 +56,18 @@ class TestArxiv(unittest.TestCase):
             self.assertEqual(parsed, output_data)
 
 
-class TextArxivMulti(unittest.TestCase):
+class TextDublinCoreMulti(unittest.TestCase):
     def setUp(self):
         stubdata_dir = os.path.join(os.path.dirname(__file__), "stubdata/")
         self.inputdir = os.path.join(stubdata_dir, "input")
         self.outputdir = os.path.join(stubdata_dir, "output")
 
-    def test_arxiv_multi(self):
+    def test_dubcore_multi(self):
         filenames = [
             "arxiv_multi_20230125",
         ]
 
-        parser = arxiv.MultiArxivParser()
+        parser = dubcore.MultiDublinCoreParser()
 
         for f in filenames:
             test_infile = os.path.join(self.inputdir, f + ".xml")
@@ -81,7 +82,6 @@ class TextArxivMulti(unittest.TestCase):
                 output_data_header = output_text.strip().split("\n\n")
 
             parsed = parser.parse(input_data, header=True)
-
             self.assertEqual(parsed, output_data_header)
 
             with open(test_outfile_noheader, "r") as fp:
