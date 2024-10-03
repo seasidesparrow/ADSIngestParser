7dba062a740b2b258d6d5626833411ba47639f5b
mugdhapolimera <35502000+mugdhapolimera@users.noreply.github.com>
2024-10-03 13:27:26 -0400

    ignore instructional tags in text blocks (#134)
    * ignore instructional tags in text blocks
    
    * deleted tag added by mistake
    
    * adding more test cases
    
    * fixed whitespace issues in new output files
    
    ---------
    
    Co-authored-by: Mugdha Polimera <mugdhapolimera@saos-MacBook-Pro-6.local>
    Co-authored-by: Mugdha Polimera <mugdhapolimera@dhcp-10-250-85-157.harvard.edu>

--- tests/test_jats.py
+++ tests/test_jats.py
@@ -93,6 +93,9 @@ class TestJATS(unittest.TestCase):
             "jats_no_issue_pagecount",
             "jats_nature_article_pubdatetype_1",
             "jats_springer_article_pubdatetype_2",
+            "jats_versita_index_tags_1",
+            "jats_versita_index_tags_2",
+            "jats_versita_index_tags_3",
         ]
 
         for f in filenames:
